5547a690bf7343b1e287221c0d420095c5fd91ea
Sergey Yuzepovich <yuzepovich@gmail.com>
2014-11-16 16:29:59 +0300

    Initial Commit

---
+++ CircularSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircularSlider
+//
+//  Created by Sergey Yuzepovich on 16.11.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
